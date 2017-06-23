7be508ef9aeb03f37e89dab57180ce8a0765944e
Blake Dixon <blaketdixon11@gmail.com>
2017-06-23 14:59:27 -0700

    changes in js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var calculation = document.querySelector("h5");
 var savedNumber = 0;
 var tempCalculation = 0;
 var state = "";
+var restartState = false;
 console.log(calculation);
 const zero = document.querySelector(".zero");
 const one = document.querySelector(".one");
@@ -31,12 +32,24 @@ this.division = this.division.bind(this);
 this.clearCalc = this.clearCalc.bind(this);
 this.aClear = this.aClear.bind(this);
 this.equalsOperation = this.equalsOperation.bind(this);
+this.flipSign = this.flipSign.bind(this);
+this.appendDecimal = this.appendDecimal.bind(this);
+function flipSign() {
+  const currentValue = parseFloat(calculation.textContent);
+  calculation.textContent = "" + currentValue / -1;
+}
+function appendDecimal() {
+  calculation.textContent += ".";
+}
 function updateCalc() {
   console.log(clickVal);
-  if (calculation.textContent.length >= 18) return;
-  if (calculation.textContent == "0")
+  if (calculation.textContent.length >= 18) {
+    return;
+  } else if (restartState) {
     calculation.textContent = clickVal;
-  else if (calculation.textContent.length >= 14) {
+  } else if (calculation.textContent == "0") {
+    calculation.textContent = clickVal;
+  } else if (calculation.textContent.length >= 14) {
     const style = window.getComputedStyle(calculation, null).getPropertyValue('font-size');
     const fontSize = parseFloat(style);
     calculation.style.fontSize = (fontSize - 2) + 'px';
@@ -54,6 +67,8 @@ function aClear() {
   state = "";
   savedNumber = 0;
   tempCalculation = 0;
+  state = ""
+  restartState = false;
   add.style.backgroundColor = "orange";
   add.style.color = "white";
   sub.style.backgroundColor = "orange"
@@ -65,71 +80,164 @@ function aClear() {
 }
 function equalsOperation() {
   console.log("Equals Called");
-  tempCalculation = savedNumber;
   switch (state) {
     case "add":
+      if (add.style.backgroundColor == "black") {
+        tempCalculation = parseFloat(calculation.textContent);
+      }
       add.style.backgroundColor = "orange";
       add.style.color = "white";
-      savedNumber = parseFloat(calculation.textContent);
-      calculation.textContent = savedNumber + tempCalculation;
+      savedNumber += tempCalculation;
+      calculation.textContent = "" + savedNumber;
       break;
     case "subtraction":
+      if (sub.style.backgroundColor == "black") {
+        tempCalculation = parseFloat(calculation.textContent);
+      }
       sub.style.backgroundColor = "orange"
       sub.style.color = "white";
-      calculation.textContent = savedNumber - parseFloat(calculation.textContent);
+      savedNumber -= tempCalculation;
+      calculation.textContent = "" + savedNumber;
       break;
     case "division":
-      divide.style.backgroundColor = "orange"
+      if (divide.style.backgroundColor == "black") {
+        tempCalculation = parseFloat(calculation.textContent);
+      }
+      divide.style.backgroundColor = "orange";
       divide.style.color = "white";
-      calculation.textContent = savedNumber / parseFloat(calculation.textContent);
+      savedNumber /= tempCalculation;
+      calculation.textContent = "" + savedNumber;
       break;
     case "times":
+      if (times.style.backgroundColor == "black") {
+        tempCalculation = parseFloat(calculation.textContent);
+      }
       times.style.backgroundColor = "orange";
       times.style.color = "white";
-      calculation.textContent = parseFloat(calculation.textContent) * savedNumber;
-      break;
+      savedNumber *= tempCalculation;
+      calculation.textContent = "" + savedNumber;
+    break;
      default:
        state = "";
        break;
   }
-  // let solve
+}
+function checkState (value) {
+  switch (value) {
+    case "add":
+      if (state == "subtraction" || state == "division" || state == "multiplication") {
+        state == "";
+        sub.style.backgroundColor = "orange"
+        sub.style.color = "white";
+        divide.style.backgroundColor = "orange"
+        divide.style.color = "white";
+        times.style.backgroundColor = "orange";
+        times.style.color = "white";
+      }
+      break;
+    case "subtraction":
+      if (state == "add" || state == "division" || state == "multiplication") {
+        state == "";
+        add.style.backgroundColor = "orange"
+        add.style.color = "white";
+        divide.style.backgroundColor = "orange"
+        divide.style.color = "white";
+        times.style.backgroundColor = "orange";
+        times.style.color = "white";
+      }
+      break;
+    case "division":
+      if (state == "subtraction" || state == "add" || state == "multiplication") {
+        state == "";
+        sub.style.backgroundColor = "orange"
+        sub.style.color = "white";
+        add.style.backgroundColor = "orange"
+        add.style.color = "white";
+        times.style.backgroundColor = "orange";
+        times.style.color = "white";
+      }
+      break;
+    case "multiplication":
+      if (state == "subtraction" || state == "division" || state == "add") {
+        state == "";
+        sub.style.backgroundColor = "orange"
+        sub.style.color = "white";
+        divide.style.backgroundColor = "orange"
+        divide.style.color = "white";
+        add.style.backgroundColor = "orange";
+        add.style.color = "white";
+      }
+      break;
+    default:
+      break;
+  }
+  if (state == "add" || state == "subtraction" || state == "division" || state == "multiplication") {
+    state == "";
+    add.style.backgroundColor = "orange";
+    add.style.color = "white";
+    sub.style.backgroundColor = "orange"
+    sub.style.color = "white";
+    divide.style.backgroundColor = "orange"
+    divide.style.color = "white";
+    times.style.backgroundColor = "orange";
+    times.style.color = "white";
+  }
 }
 function addition() {
   console.log("Addition Called");
+  checkState("add");
   add.style.backgroundColor = "black";
   add.style.color = "orange";
   if (savedNumber == 0) {
     savedNumber = parseFloat(calculation.textContent);
   } else {
-
+    savedNumber += parseFloat(calculation.textContent);
+    calculation.textContent = "" + savedNumber;
   }
-  tempCalculation = savedNumber;
-  clearCalc();
   state = "add";
+  restartState = true;
 }
 function subtraction() {
   console.log("Subtraction Called");
+  checkState("subtraction");
   sub.style.backgroundColor = "black";
   sub.style.color = "orange";
-  savedNumber = parseFloat(calculation.textContent);
-  clearCalc();
+  if (savedNumber == 0) {
+    savedNumber = parseFloat(calculation.textContent);
+  } else {
+    savedNumber -= parseFloat(calculation.textContent);
+    calculation.textContent = "" + savedNumber;
+  }
   state = "subtraction";
+  restartState = true;
 }
 function division() {
   console.log("division Called");
+  checkState("division");
   divide.style.backgroundColor = "black";
   divide.style.color = "orange";
-  savedNumber = parseFloat(calculation.textContent);
-  clearCalc();
+  if (savedNumber == 0) {
+    savedNumber = parseFloat(calculation.textContent);
+  } else {
+    savedNumber /= parseFloat(calculation.textContent);
+    calculation.textContent = "" + savedNumber;
+  }
   state = "division";
+  restartState = true;
 }
 function multiplication() {
   console.log("Times Called");
+  checkState("multiplication");
   times.style.backgroundColor = "black";
   times.style.color = "orange";
-  savedNumber = parseFloat(calculation.textContent);
-  clearCalc();
+  if (savedNumber == 0) {
+    savedNumber = parseFloat(calculation.textContent);
+  } else {
+    savedNumber *= parseFloat(calculation.textContent);
+    calculation.textContent = "" + savedNumber;
+  }
   state = "times";
+  restartState = true;
 }
 zero.addEventListener('click', function () { clickVal = "0" });
 zero.addEventListener('click', this.updateCalc);
@@ -158,5 +266,5 @@ add.addEventListener('click', this.addition);
 sub.addEventListener('click', this.subtraction);
 times.addEventListener('click', this.multiplication);
 divide.addEventListener('click', this.divison);
-//decimal
-//sign
+decimal.addEventListener('click', this.appendDecimal);
+sign.addEventListener('click', this.flipSign);
