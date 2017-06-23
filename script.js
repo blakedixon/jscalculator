eed21d0b2f4aace14cf686da0feedcb4606a50f8
Blake Dixon <blaketdixon11@gmail.com>
2017-06-23 14:04:25 -0700

    worked on js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var clickVal = "";
 var calculation = document.querySelector("h5");
-var secondCalculation = 0;
+var savedNumber = 0;
+var tempCalculation = 0;
 var state = "";
 console.log(calculation);
-
 const zero = document.querySelector(".zero");
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
@@ -17,18 +17,20 @@ const nine = document.querySelector(".nine");
 const times = document.querySelector(".times");
 const add = document.querySelector(".add");
 const clear = document.querySelector(".clear");
+const allClear = document.querySelector(".allClear");
 const sign = document.querySelector(".sign");
 const decimal = document.querySelector(".decimal");
-const divison = document.querySelector(".divison");
-const subtraction = document.querySelector(".subtraction");
+const divide = document.querySelector(".division");
+const sub = document.querySelector(".subtraction");
 const equals = document.querySelector(".equals");
-
-
 this.updateCalc = this.updateCalc.bind(this);
 this.addition = this.addition.bind(this);
+this.subtraction = this.subtraction.bind(this);
+this.multiplication = this.multiplication.bind(this);
+this.division = this.division.bind(this);
 this.clearCalc = this.clearCalc.bind(this);
+this.aClear = this.aClear.bind(this);
 this.equalsOperation = this.equalsOperation.bind(this);
-
 function updateCalc() {
   console.log(clickVal);
   if (calculation.textContent.length >= 18) return;
@@ -37,7 +39,6 @@ function updateCalc() {
   else if (calculation.textContent.length >= 14) {
     const style = window.getComputedStyle(calculation, null).getPropertyValue('font-size');
     const fontSize = parseFloat(style);
-    // now you have a proper float for the font size (yes, it can be a float, not just an integer)
     calculation.style.fontSize = (fontSize - 2) + 'px';
     calculation.textContent += clickVal;
   } else {
@@ -48,25 +49,88 @@ function clearCalc() {
   calculation.textContent = "0";
   calculation.style.fontSize = "24px";
 }
+function aClear() {
+  clearCalc();
+  state = "";
+  savedNumber = 0;
+  tempCalculation = 0;
+  add.style.backgroundColor = "orange";
+  add.style.color = "white";
+  sub.style.backgroundColor = "orange"
+  sub.style.color = "white";
+  divide.style.backgroundColor = "orange"
+  divide.style.color = "white";
+  times.style.backgroundColor = "orange";
+  times.style.color = "white";
+}
 function equalsOperation() {
   console.log("Equals Called");
+  tempCalculation = savedNumber;
   switch (state) {
     case "add":
-      calculation.textContent = parseFloat(calculation.textContent) + secondCalculation;
+      add.style.backgroundColor = "orange";
+      add.style.color = "white";
+      savedNumber = parseFloat(calculation.textContent);
+      calculation.textContent = savedNumber + tempCalculation;
+      break;
+    case "subtraction":
+      sub.style.backgroundColor = "orange"
+      sub.style.color = "white";
+      calculation.textContent = savedNumber - parseFloat(calculation.textContent);
+      break;
+    case "division":
+      divide.style.backgroundColor = "orange"
+      divide.style.color = "white";
+      calculation.textContent = savedNumber / parseFloat(calculation.textContent);
+      break;
+    case "times":
+      times.style.backgroundColor = "orange";
+      times.style.color = "white";
+      calculation.textContent = parseFloat(calculation.textContent) * savedNumber;
+      break;
+     default:
+       state = "";
+       break;
   }
-
   // let solve
 }
-
 function addition() {
   console.log("Addition Called");
   add.style.backgroundColor = "black";
   add.style.color = "orange";
-  secondCalculation = parseFloat(calculation.textContent);
+  if (savedNumber == 0) {
+    savedNumber = parseFloat(calculation.textContent);
+  } else {
+
+  }
+  tempCalculation = savedNumber;
   clearCalc();
   state = "add";
 }
-
+function subtraction() {
+  console.log("Subtraction Called");
+  sub.style.backgroundColor = "black";
+  sub.style.color = "orange";
+  savedNumber = parseFloat(calculation.textContent);
+  clearCalc();
+  state = "subtraction";
+}
+function division() {
+  console.log("division Called");
+  divide.style.backgroundColor = "black";
+  divide.style.color = "orange";
+  savedNumber = parseFloat(calculation.textContent);
+  clearCalc();
+  state = "division";
+}
+function multiplication() {
+  console.log("Times Called");
+  times.style.backgroundColor = "black";
+  times.style.color = "orange";
+  savedNumber = parseFloat(calculation.textContent);
+  clearCalc();
+  state = "times";
+}
 zero.addEventListener('click', function () { clickVal = "0" });
 zero.addEventListener('click', this.updateCalc);
 one.addEventListener('click', function () { clickVal = "1" });
@@ -87,7 +151,12 @@ eight.addEventListener('click', function () { clickVal = "8" });
 eight.addEventListener('click', this.updateCalc);
 nine.addEventListener('click', function () { clickVal = "9" });
 nine.addEventListener('click', this.updateCalc);
-
 clear.addEventListener('click', this.clearCalc);
+allClear.addEventListener('click', this.aClear);
 equals.addEventListener('click', this.equalsOperation);
 add.addEventListener('click', this.addition);
+sub.addEventListener('click', this.subtraction);
+times.addEventListener('click', this.multiplication);
+divide.addEventListener('click', this.divison);
+//decimal
+//sign
