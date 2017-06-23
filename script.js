80bf1336e618aaaf171e7506c1f0be56a88e75c9
Blake Dixon <blaketdixon11@gmail.com>
2017-06-22 22:26:11 -0700

    addition working! fix button highlighting

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var clickVal = "";
 var calculation = document.querySelector("h5");
+var secondCalculation = 0;
+var state = "";
 console.log(calculation);
 
 const zero = document.querySelector(".zero");
 const one = document.querySelector(".one");
-const second = document.querySelector(".second");
+const two = document.querySelector(".two");
 const three = document.querySelector(".three");
 const four = document.querySelector(".four");
 const five = document.querySelector(".five");
@@ -19,31 +21,58 @@ const sign = document.querySelector(".sign");
 const decimal = document.querySelector(".decimal");
 const divison = document.querySelector(".divison");
 const subtraction = document.querySelector(".subtraction");
+const equals = document.querySelector(".equals");
 
 
 this.updateCalc = this.updateCalc.bind(this);
 this.addition = this.addition.bind(this);
+this.clearCalc = this.clearCalc.bind(this);
+this.equalsOperation = this.equalsOperation.bind(this);
 
 function updateCalc() {
   console.log(clickVal);
+  if (calculation.textContent.length >= 18) return;
   if (calculation.textContent == "0")
     calculation.textContent = clickVal;
-  else
+  else if (calculation.textContent.length >= 14) {
+    const style = window.getComputedStyle(calculation, null).getPropertyValue('font-size');
+    const fontSize = parseFloat(style);
+    // now you have a proper float for the font size (yes, it can be a float, not just an integer)
+    calculation.style.fontSize = (fontSize - 2) + 'px';
     calculation.textContent += clickVal;
+  } else {
+    calculation.textContent += clickVal;
+  }
+}
+function clearCalc() {
+  calculation.textContent = "0";
+  calculation.style.fontSize = "24px";
+}
+function equalsOperation() {
+  console.log("Equals Called");
+  switch (state) {
+    case "add":
+      calculation.textContent = parseFloat(calculation.textContent) + secondCalculation;
+  }
+
+  // let solve
 }
 
 function addition() {
   console.log("Addition Called");
   add.style.backgroundColor = "black";
   add.style.color = "orange";
+  secondCalculation = parseFloat(calculation.textContent);
+  clearCalc();
+  state = "add";
 }
 
 zero.addEventListener('click', function () { clickVal = "0" });
 zero.addEventListener('click', this.updateCalc);
 one.addEventListener('click', function () { clickVal = "1" });
 one.addEventListener('click', this.updateCalc);
-second.addEventListener('click', function () { clickVal = "2" });
-second.addEventListener('click', this.updateCalc);
+two.addEventListener('click', function () { clickVal = "2" });
+two.addEventListener('click', this.updateCalc);
 three.addEventListener('click', function () { clickVal = "3" });
 three.addEventListener('click', this.updateCalc);
 four.addEventListener('click', function () { clickVal = "4" });
@@ -59,5 +88,6 @@ eight.addEventListener('click', this.updateCalc);
 nine.addEventListener('click', function () { clickVal = "9" });
 nine.addEventListener('click', this.updateCalc);
 
-clear.addEventListener('click', function () { calculation.textContent = "0" });
+clear.addEventListener('click', this.clearCalc);
+equals.addEventListener('click', this.equalsOperation);
 add.addEventListener('click', this.addition);
